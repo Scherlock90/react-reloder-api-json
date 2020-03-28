cc457240eab83c20c79fae2dde860feb47c2df94
Sebastian Napora <sebastian.napora@intive.com>
2020-03-28 15:33:54 +0100

    change in Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
 import React from 'react'
 
-export const Header = () => <div className="title-page">JSON API LOADER <br /> WITH BLACK&WHITE EFFECTS</div>
+export const Header = () =>
+    <div className="title-page">JSON API LOADER <br /> WITH BLACK&WHITE EFFECTS</div>
