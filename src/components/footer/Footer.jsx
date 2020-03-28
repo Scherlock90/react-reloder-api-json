58af01b6a60c4226e15105182e47a96f08c580ef
Sebastian Napora <sebastian.napora@intive.com>
2020-03-28 15:42:38 +0100

    delet not used code

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,3 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 
-export const Footer = () =>
-    <div className="footer">
-        <Button variant="contained" color="primary">Designed SeN</Button>
-    </div>
+export const Footer = () => <div className="footer">Designed SeN</div>
